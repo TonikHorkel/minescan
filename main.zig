75fbb0bdd0638ddb45c856d78873dceeb0e9c45d
Tonda Horkel <59200680+TonikHorkel@users.noreply.github.com>
2023-03-05 20:59:06 +0100

    Create main.zig

---
+++ main.zig
@@ -0,0 +1,26 @@
+const std = @import("std");
+const mem = std.mem;
+const os = std.os;
+
+pub fn main() !void {
+    const sock_timeout = os.timeval {
+        .tv_sec = 10,
+        .tv_usec = 0,
+    };
+    const sock_addr = os.sockaddr.in {
+        .family = os.AF.INET,
+        .addr = 0xC0A80001,
+        .port = comptime mem.nativeToBig(u16, 25565),
+    };
+    const sock_fd = try os.socket(os.AF.INET, os.SOCK.STREAM | os.SOCK.NONBLOCK, 0);
+    errdefer os.closeSocket(sock_fd);
+    try os.setsockopt(sock_fd, os.SOL.SOCKET, os.SO.RCVTIMEO, comptime &mem.toBytes(sock_timeout));
+    try os.setsockopt(sock_fd, os.SOL.SOCKET, os.SO.SNDTIMEO, comptime &mem.toBytes(sock_timeout));
+    try os.connect(sock_fd, @ptrCast(*const os.sockaddr, &sock_addr), @sizeOf(os.sockaddr.in));
+//    catch |err| switch (err) {
+//        error.ConnectionPending => {
+//            return err;
+//        },
+//        else => return err,
+//    };
+}
